26f72efe35f630600f74f80195d78f5fd6edcf5b
Grush-cloud <georgeashinze@gmail.com>
2021-05-30 12:38:22 +0400

    lessons file added

---
+++ lessons.js
@@ -0,0 +1,102 @@
+lessons=[
+    {
+    id:1001,
+    subject: "Art",
+    location: "London",
+    price: 500,
+    priceicon: '$',
+    spaces:5,
+    image:"images/art.jpg",
+    
+},
+{
+    id:1002,
+    subject: "Biology",
+    location: "United States",
+    price: 200,
+    priceicon: '$',
+    spaces:5,
+    image:"images/biology.jpg",
+    
+},
+{
+    id:1003,
+    subject: "Chemistry",
+    location: "London",
+    price: 1000,
+    priceicon: '$',
+    spaces:5,
+    image:"images/chem.jpg",
+    
+},
+{
+    id:1004,
+    subject: "English",
+    location: "United Kingdom",
+    price: 2200,
+    priceicon: '$',
+    spaces:5,
+    image:"images/english.jpg",
+    
+},
+{
+    id:1005,
+    subject: "French",
+    location: "Paris",
+    price: 2000,
+    priceicon: '$',
+    spaces:5,
+    image:"images/french.jpg",
+    
+},
+{
+    id:1006,
+    subject: "Geography",
+    location: "London",
+    price: 2030,
+    priceicon: '$',
+    spaces:5,
+    image:"images/geography.jpg",
+    
+},
+{
+    id:1007,
+    subject: "History",
+    location: "Ghana",
+    price: 1000,
+    priceicon: '$',
+    spaces:5,
+    image:"images/history.jpg",
+    
+},
+{
+    id:1008,
+    subject: "Maths",
+    location: "London",
+    price: 2400,
+    priceicon: '$',
+    spaces:5,
+    image:"images/maths.jpg",
+    
+},
+{
+    id:1009,
+    subject: "Music",
+    location: "Paris",
+    price: 234,
+    priceicon: '$',
+    spaces:5,
+    image:"images/music.jpg",
+    
+},
+{
+    id:1010,
+    subject: "Physics",
+    location: "London",
+    price: 1400,
+    priceicon: '$',
+    spaces:5,
+    image:"images/physics.jpg",
+    
+},
+]
